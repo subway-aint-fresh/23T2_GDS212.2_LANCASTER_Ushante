93fe7834e97e8b19c89e5e6437fd298137304622
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-06-14 01:47:20 +1000

    Updated GameManager.cs
    - Timer updates text and throws up game end in debug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,22 +8,47 @@ public class GameManager : MonoBehaviour
 {
     public float timeLimit = 60f; // time limit set in seconds
     private float currentTime; // time remaining
-    private TextMeshProUGUI timerText; // reference to time text using tmp
+    private TextMeshProUGUI timerText; // reference to the tmp time text
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        timerText = GetComponent<TextMeshProUGUI>();
+        StartTimer();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (currentTime > 0f)
+        {
+            currentTime -= Time.deltaTime;
+            UpdateTimerText();
+        } 
+        else
+        {
+            currentTime = 0f;
+            Debug.Log("time out");
+            //execute game finished
+        }
     }
 
-    //if timer runs out, execute homescreen method
+    //Timer
+
+        public void StartTimer()
+        {
+            currentTime = timeLimit;
+        }
+
+        private void UpdateTimerText()
+        {
+            string minutes = Mathf.Floor(currentTime / 60f).ToString("00");
+            string seconds = (currentTime % 60f).ToString("00");
+            timerText.text = $"{minutes}:{seconds}";
+        }
+
+    //game finished method, throws up fail window
 
     //Sends player to the homescreen/ homescene
     public void HomeScreen()
