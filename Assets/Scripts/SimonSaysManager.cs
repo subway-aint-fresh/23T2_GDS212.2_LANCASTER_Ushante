4acb0f69e5085e43f6f9cf3ba9298835dd8a2295
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-06-15 22:03:01 +1000

    Adding button pressing logic
    - when a button is pressed by the player it will switch to it's colored state

--- Assets/Scripts/SimonSaysManager.cs
+++ Assets/Scripts/SimonSaysManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class SimonSaysManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject button1Color;         // Reference to the colored child object of exit button 1
+    public GameObject button1;              // Reference to the non-colored child object of exit button 1
+
+    public GameObject button2Color;         // Button 2 colored child
+    public GameObject button2;              // Button 2 non-colored child
+    
+    public GameObject button3Color;         // Button 3 colored child
+    public GameObject button3;              // Button 3 non-colored child
+
+    public GameObject button4Color;         // Button 4 colored child
+    public GameObject button4;              // Button 4 non-colored child
+
     void Start()
     {
-        
+        // Set the initial state of the colored versions of the buttons
+        button1Color.SetActive(false);
+        button2Color.SetActive(false);
+        button3Color.SetActive(false);
+        button4Color.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void HandlePlayerInput(int buttonIndex)
+    {
+        switch (buttonIndex)
+        {
+            case 1:
+                button1.GetComponent<SpriteRenderer>().enabled = false;
+                button1Color.SetActive(true);
+                break;
+            case 2:
+                button2.GetComponent<SpriteRenderer>().enabled = false;
+                button2Color.SetActive(true);
+                break;
+            case 3:
+                button3.GetComponent<SpriteRenderer>().enabled = false;
+                button3Color.SetActive(true);
+                break;
+            case 4:
+                button4.GetComponent<SpriteRenderer>().enabled = false;
+                button4Color.SetActive(true);
+                break;
+        }
+    }
+
 }
